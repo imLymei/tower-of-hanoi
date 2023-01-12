fa909c6d3e6ae7ac1861352609c491e55e8d9ff2
Lymei <ronaldofelipespm@bol.com.br>
2023-01-12 16:07:06 -0300

    Footer update

--- app/Footer.jsx
+++ app/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
 	return (
-		<div className='absolute left-1/2 translate-x-[-50%] bottom-[-230px] sm:bottom-6 text-white/10'>
+		<div className='absolute left-1/2 translate-x-[-50%] bottom-[-230px] py-5 sm:bottom-6 text-white/10'>
 			<a href='https://github.com/imLymei' target='_blank'>
 				Felipe Cardoso©2022
 			</a>
